582c6d5835eb4d434a64aa05714c9fed38b4fda4
loverzai <loverzai@users.noreply.github.com>
2022-09-23 01:30:44 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,6 @@
 function 移动 (dx: number, dy: number) {
+    x2 = x
+    y2 = y
     x = x + dx
     y = y + dy
     if (x > 4) {
@@ -11,16 +13,29 @@ function 移动 (dx: number, dy: number) {
     } else if (y < 0) {
         y = 5
     }
+    if (sprite.get(LedSpriteProperty.X) == sprite2.get(LedSpriteProperty.X) && sprite.get(LedSpriteProperty.Y) == sprite2.get(LedSpriteProperty.Y)) {
+        x2 = x
+        y2 = y
+    } else {
+        sprite2.set(LedSpriteProperty.X, x2)
+        sprite2.set(LedSpriteProperty.Y, y2)
+    }
     sprite.set(LedSpriteProperty.X, x)
     sprite.set(LedSpriteProperty.Y, y)
     basic.pause(500)
 }
+let sprite2: game.LedSprite = null
 let sprite: game.LedSprite = null
+let y2 = 0
+let x2 = 0
 let y = 0
 let x = 0
 x = 2
 y = 2
+x2 = 2
+y2 = 2
 sprite = game.createSprite(x, y)
+sprite2 = game.createSprite(x2, y2)
 pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
